1c4195921f98f2031b2a09c7040d03c10e3f81bb
PersusPrince <perseus.prince070@gmail.com>
2023-10-30 02:21:02 +0100

    Connect To MongoDB Database

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,15 @@
 import express from "express";
 import data from "./data.js";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('connected to db'))
+    .catch((err) => {
+        console.log(err.message)
+    })
 
 const app = express();
 
@@ -24,6 +34,7 @@ app.get('/api/products/:id', (req, res) => {
         res.status(404).send({ message: 'Product Not Found'})
     }
 })
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
