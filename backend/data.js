ebd26db5bf53c5d254be49a15ad70ae5c876945a
PersusPrince <perseus.prince070@gmail.com>
2023-10-30 07:38:47 +0100

    Seed Sample Users

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+    users: [
+        {
+            name: 'Pumba',
+            email: 'blackants510@gmail.com',
+            password: bcrypt.hashSync('123456'),
+            isAdmin: true
+        },
+        {
+            name: 'John',
+            email: 'user@example.com',
+            password: bcrypt.hashSync('123456'),
+            isAdmin: false
+        }
+    ],
     products: [
         {
             // _id: '1',
